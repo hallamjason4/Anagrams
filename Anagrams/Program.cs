a4437ead6932a1ce747d7cc3cf59d8a8f0e7649f
hallamjason4 <70266362+hallamjason4@users.noreply.github.com>
2021-02-18 16:09:27 +0000

    Add files via upload

---
+++ Anagrams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Anagrams
+    {
+        static void Main(string[] args)
+        {
+            string str = "parts,traps,arts,rats,starts,tarts,rat,art,tar,tars,stars,stray";
+            string[] splitAnags = str.Split(',');
+            string targetString = "star";
+            string targetStringOrd = String.Concat(targetString.OrderBy(y => y));
+
+            foreach (var item in splitAnags)
+            {
+                string order = item;
+                string x = String.Concat(order.OrderBy(y => y));
+                if (x == targetStringOrd){
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
